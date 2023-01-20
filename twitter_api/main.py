001c486c3a9a89ec88215a6e4f43e5c9b02c2aed
Mansur Agliev <93724429+agliev@users.noreply.github.com>
2023-01-20 16:30:58 +0300

    MVP commit

--- twitter_api/main.py
+++ twitter_api/main.py
@@ -1,16 +1,4 @@
-from selenium.webdriver import Chrome
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from time import sleep
-import random
-import json
-import requests
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-import pickle
-from secrets import LOGIN, NAME, PASSWORD, PHONE_NUMBER
-from secrets import follow_path, acsept_cookies, first_retwit_path, second_retwit_path, like_path 
+from secret import LOGIN, NAME, PASSWORD, PHONE_NUMBER
 from source import TwitterBot
 
 
@@ -21,11 +9,10 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
 
-bot = TwitterBot(LOGIN, NAME, PASSWORD, PHONE_NUMBER)
+def main():
+    
+    bot = TwitterBot(LOGIN, NAME, PASSWORD, PHONE_NUMBER, headless=False)
+    bot.like_and_retwit(follow=False)
 
-# bot.authorise()
-
-bot.follow('https://twitter.com/KaoryuTamago')
-
-
-
+if __name__ == '__main__':
+    main()
--- twitter_api/source.py
+++ twitter_api/source.py
@@ -3,28 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 import random
-from fake_useragent import UserAgent
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 import pickle
-from secrets import follow_path, acsept_cookies, first_retwit_path, second_retwit_path, like_path 
-from secrets import BROWSER_PATH
+from secret import follow_path, acsept_cookies, first_retwit_path, second_retwit_path, like_path 
+from secret import BROWSER_PATH
 
 
 options = webdriver.ChromeOptions()
 
 # changing user-agent
 # options.add_argument(f"user-agent={UserAgent(browsers=['chrome']).random}")
-options.add_argument(f"user-agent=qwerty")
+options.add_argument(f"user-agent=asdfg")
 
 options.add_argument('--incognito')
 
 # make it human
 options.add_argument("--disable-blink-features=AutomationControlled")
-
-# executing without browser opening
-# options.add_argument("--headless")
 
 # for Networks list parsing
 options.set_capability("goog:loggingPrefs", {"performance": "ALL", "browser": "ALL"}) 
@@ -32,13 +28,23 @@ options.set_capability("goog:loggingPrefs", {"performance": "ALL", "browser": "A
 
 class TwitterBot():
 
-    def __init__(self,login:str, name:str, password:str, phone_number:str):
+    def __init__(self,login:str, name:str, password:str, phone_number:str,headless:bool=False):
 
         self.login = login
         self.name = name
         self.password = password
         self.phone_number = phone_number
+        
+        if headless: # Добавление аргумента для работы без открытия браузерного окна
+
+            # executing without browser opening
+            options.add_argument("--headless")
+
+        else:
+            pass
+
         self.bot = Chrome(BROWSER_PATH,options=options)
+
     
 
 
@@ -94,7 +100,7 @@ class TwitterBot():
             print('authentication complete!')
 
         # cookies
-        pickle.dump(browser.get_cookies(), open(f"Downloads/twitter_api/cookies/{self.login}_cookies", "wb"))
+        pickle.dump(browser.get_cookies(), open(f"C:/Users/Mans/Downloads/twitter_api/cookies/{self.login}_cookies", "wb")) 
         print("🍪Cookies received!🍪")
 
         browser.close()
@@ -108,9 +114,9 @@ class TwitterBot():
         
         browser = self.bot
         browser.get(url)
-        sleep(5*random.random()+2)
+        sleep(random.random()+2)
 
-        for cookie in pickle.load(open(f"Downloads/twitter_api/cookies/{self.login}_cookies", "rb")):
+        for cookie in pickle.load(open(f"C:/Users/Mans/Downloads/twitter_api/cookies/{self.login}_cookies", "rb")):
             browser.add_cookie(cookie)
 
         sleep(5*random.random()+2)
@@ -125,10 +131,130 @@ class TwitterBot():
         WebDriverWait(browser, 15*random.random()).until(expected_conditions.visibility_of_element_located((By.XPATH, follow_path)))
         browser.find_element(By.XPATH,follow_path).click()
 
+
+
+
+    def like_and_retwit(self, follow:bool=False):
+        
+        browser = self.bot
+        browser.get('https://www.premint.xyz/kaoryu-tamago/')
+        sleep(2+random.random())
+
+        tweet_link = browser.find_element(By.LINK_TEXT, 'this tweet').get_attribute('href')
+        
+        
+        channel_link_items = browser.find_elements(By.CLASS_NAME, 'c-base-1')
+        channel_link = channel_link_items[2].get_attribute('href')
+
+        browser.get(channel_link)
+        sleep(3*random.random())
+
+        for cookie in pickle.load(open(f"C:/Users/Mans/Downloads/twitter_api/cookies/{self.login}_cookies", "rb")):
+            browser.add_cookie(cookie)
+        sleep(3*random.random())
+
+        browser.refresh()
+        sleep(2+random.random())
+
+        if follow:
+
+            try:
+                WebDriverWait(browser, 15*random.random()).until(expected_conditions.visibility_of_element_located((By.XPATH, follow_path)))
+                browser.find_element(By.XPATH, follow_path).click()
+                sleep(random.random())
+                
+                print('Follow complete')
+
+            except:
+                print('Follow items not found!')     
+        
+        else:
+            print("Following isn't nessesary")
+
+        browser.get(tweet_link)
+        sleep(3+random.random())
+
+        # Избавимся от уведомления об использовании cookie
+        WebDriverWait(browser, 10*random.random()).until(expected_conditions.visibility_of_element_located((By.XPATH, acsept_cookies)))
+        browser.find_element(By.XPATH,acsept_cookies).click()
+        
+        
+        browser.execute_script(f"window.scrollTo({random.randint(0, 29)},{502+random.randint(305, 467)})")
+        sleep(3+5*random.random())
+
+        try:
+            items = browser.find_elements(By.CLASS_NAME, "r-1srniue")
+            print('Items found')
+
+            try:
+                # Ретвитнем пост
+                items[1].click() 
+                sleep(random.random())
+
+                # Подтвердим ретвит
+                WebDriverWait(browser, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, second_retwit_path)))
+                browser.find_element(By.XPATH,second_retwit_path).click()
+                print('Retwit complete')
+
+                sleep(2*random.random())
+                
+            except:
+                print("Retwit error!")   
+
+            try:
+                # Лайкнем пост
+                items[2].click() 
+                sleep(random.random())
+                print('Like complete')
+
+                sleep(2*random.random())
+                
+            except:
+                print("Like error!")
+        
+        except:
+            print('Items not found!')
+        
+        # Завершив подготовку - авторизуемся в конкурсе 
+        browser.get('https://www.premint.xyz/kaoryu-tamago/')
+        sleep(10+random.random())
+        
+        try:
+            # Попытка регистрации
+            button_items = browser.find_elements(By.CLASS_NAME,'btn')
+            button_items[5].click()
+
+            sleep(10 + 5*random.random())
+            print('Premint.xyz authorisation complete')
+
+        except:
+            print("Let's authorise by Twiter")
+            # Необходимо авторизоваться через Twitter
+            try:
+                # Входим на страницу авторизации
+                browser.get('https://www.premint.xyz/login/?next=/kaoryu-tamago/')
+                sleep(5 + 5*random.random())
+                
+                # Выбираем авторизацию черех Twitter
+                browser.find_elements(By.CLASS_NAME,'fa-twitter').click()
+                sleep(5+random.random())
+
+                # Новая попытка регистрации
+                browser.get('https://www.premint.xyz/kaoryu-tamago/')
+                sleep(6+random.random())
+
+                button_items = browser.find_elements(By.CLASS_NAME,'btn')
+                button_items[5].click()
+
+                sleep(10 + 5*random.random())
+                print('Premint.xyz authorisation complete')
+            
+            except:
+                print("Premint.xyz not authorised!")
+
         sleep(20)
         browser.close()
         browser.quit()
 
-    def like_and_retwit():
-        pass
+
 
