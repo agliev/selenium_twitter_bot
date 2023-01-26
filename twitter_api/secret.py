b230f107f638ac02a664bc7698d94cbd613311c0
Mansur Agliev <93724429+agliev@users.noreply.github.com>
2023-01-27 01:48:28 +0300

    secret update

---
+++ twitter_api/secret.py
@@ -0,0 +1,77 @@
+import pandas as pd
+
+
+LOGIN = ''
+PASSWORD = ''
+NAME = ''
+
+BROWSER_PATH = ''
+TWITTER_URL = 'https://twitter.com/login'
+PHONE_NUMBER = ''
+
+follow_path = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/div/div/div[1]/div[2]/div/div[1]/div/div/span/span'
+acsept_cookies = '/html/body/div[1]/div/div/div[1]/div/div/div/div/div/div[2]/div[1]/div/span/span'
+first_retwit_path = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div/div[3]/div/div/div[2]/span/span/span'
+second_retwit_path = '/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div[2]/div/div[3]/div/div/div/div/div[2]/div/span'
+like_path = '/html/body/div[1]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div/section/div/div/div[1]/div/div/div/article/div/div/div/div[2]/div[2]/div[2]/div[3]/div/div[4]/div/div/div[2]/span/span/span'
+
+user_agents = list(pd.read_csv('user_agents.csv')['User_Agent'])
+
+# IPv4 ПРОКСИ
+PROXY_HOST = ''
+PROXY_PORT = ''
+PROXY_USER = ''
+PROXY_PASS = ''
+
+
+
+
+manifest_json = """
+{
+    "version": "1.0.0",
+    "manifest_version": 2,
+    "name": "Chrome Proxy",
+    "permissions": [
+        "proxy",
+        "tabs",
+        "unlimitedStorage",
+        "storage",
+        "<all_urls>",
+        "webRequest",
+        "webRequestBlocking"
+    ],
+    "background": {
+        "scripts": ["background.js"]
+    },
+    "minimum_chrome_version":"76.0.0"
+}
+"""
+
+
+background_js = """
+let config = {
+        mode: "fixed_servers",
+        rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "%s",
+            port: parseInt(%s)
+        },
+        bypassList: ["localhost"]
+        }
+    };
+chrome.proxy.settings.set({value: config, scope: "regular"}, function() {});
+function callbackFn(details) {
+    return {
+        authCredentials: {
+            username: "%s",
+            password: "%s"
+        }
+    };
+}
+chrome.webRequest.onAuthRequired.addListener(
+            callbackFn,
+            {urls: ["<all_urls>"]},
+            ['blocking']
+);
+""" % (PROXY_HOST, PROXY_PORT, PROXY_USER, PROXY_PASS)
